03b8d1c53d237bbd3c66816785ff6757cef4ef55
hunderaweke@gmail.com <hunderaweke@gmail.com>
2023-04-03 15:59:16 +0000

    Updated the home page

---
+++ script.js
@@ -0,0 +1,6 @@
+var homeButton = document.querySelector('.home-btn')
+var navigationBar = docume.querySelector('.navigation')
+homeButton.addEventListener("click",function () {
+    console.log("clicked");
+})
+navigationBar.addEventListener('scroll')
